d044dbdadfe5640fb988893240e431981966798f
fernandadias <diasfeh@gmail.com>
2020-03-21 17:21:54 -0300

    Insere os ajustes para o content

---
+++ js/animacoes.js
@@ -0,0 +1,23 @@
+var textPath = document.querySelector('#text-path');
+
+var textContainer = document.querySelector('#text-container');
+
+var path = document.querySelector( textPath.getAttribute('href') );
+
+var pathLength = path.getTotalLength();
+
+function updateTextPathOffset(offset){
+  textPath.setAttribute('startOffset', offset); 
+}
+
+updateTextPathOffset(pathLength);
+
+function onScroll(){
+  requestAnimationFrame(function(){
+    var rect = textContainer.getBoundingClientRect();
+    var scrollPercent = rect.y / window.innerHeight;
+    updateTextPathOffset( scrollPercent * pathLength );
+  });
+}
+
+window.addEventListener('scroll',onScroll);
